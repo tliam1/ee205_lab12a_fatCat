d50d202e8935f348afd25d471e75244dac7db59c
tliam1 <tliam@hawaii.edu>
2022-04-12 23:12:26 -0500

    Initial commit

---
+++ Weight.cpp
@@ -0,0 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab12a_fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.cpp
+/// @version 1.0
+///
+/// @author Liam Tapper <tliam@hawaii.edu>
+/// @date   12_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include "Weight.h"
---
+++ Weight.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab12a_fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.h
+/// @version 1.0
+///
+/// @author Liam Tapper <tliam@hawaii.edu>
+/// @date   12_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef EE205_LAB12A_FATCAT_WEIGHT_H
+#define EE205_LAB12A_FATCAT_WEIGHT_H
+
+
+class Weight {
+
+};
+
+
+#endif //EE205_LAB12A_FATCAT_WEIGHT_H
